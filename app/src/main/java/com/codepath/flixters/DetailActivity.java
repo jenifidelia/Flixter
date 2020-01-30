4ac4dcfb18916158af1efb020545aeb95165cae5
jenifidelia <fideliaj@berea.edu>
2020-01-30 04:46:53 -0500

    unit2_secondcommit

---
+++ app/src/main/java/com/codepath/flixters/DetailActivity.java
@@ -0,0 +1,35 @@
+package com.codepath.flixters;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Parcel;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.codepath.flixters.models.Movie;
+
+import org.parceler.Parcels;
+
+public class DetailActivity extends AppCompatActivity {
+
+    TextView tvTitle;
+    TextView tvOverview;
+    RatingBar ratingBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        tvTitle = findViewById(R.id.tvTitle);
+        tvOverview = findViewById(R.id.tvOverview);
+        ratingBar = findViewById(R.id.ratingBar);
+
+        Movie movie = Parcels.unwrap(getIntent().getParcelableExtra("movie"));
+        tvTitle.setText(movie.getTitle());
+        tvOverview.setText(movie.getOverview());
+        ratingBar.setRating((float) movie.getRating());
+
+    }
+}
