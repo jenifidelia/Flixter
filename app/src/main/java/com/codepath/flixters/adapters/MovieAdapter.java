ac0b00053afdd19e85fde9d2d67982f7e4c8f256
jenifidelia <fideliaj@berea.edu>
2020-02-01 22:45:10 -0500

    almost_done1

--- app/src/main/java/com/codepath/flixters/adapters/MovieAdapter.java
+++ app/src/main/java/com/codepath/flixters/adapters/MovieAdapter.java
@@ -1,20 +1,27 @@
 package com.codepath.flixters.adapters;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.RequestBuilder;
+import com.codepath.flixters.DetailActivity;
 import com.codepath.flixters.R;
 import com.codepath.flixters.models.Movie;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -51,6 +58,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
+        RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
@@ -61,13 +69,32 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview= itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
 
         }
 
-        public void bind(Movie movie) {
+        public void bind(final Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
-            Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
+
+            String imageUrl;
+
+            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+
+                imageUrl = movie.getBackdropPath();
+            } else {
+                imageUrl = movie.getPosterPath();
+            }
+
+            Glide.with(context).load(imageUrl).into(ivPoster);
+            tvOverview.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                   Intent i = new Intent(context, DetailActivity.class);
+                   i.putExtra("movie", Parcels.wrap(movie));
+                   context.startActivity(i);
+                }
+            });
         }
     }
 }
--- app/src/main/java/com/codepath/flixters/models/Movie.java
+++ app/src/main/java/com/codepath/flixters/models/Movie.java
@@ -3,20 +3,35 @@ package com.codepath.flixters.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
+@Parcel
+
 public class Movie {
 
+
+    Integer movieId;
+    String backdropPath;
     String posterPath;
     String title;
     String overview;
+    double rating;
+
+    public Movie(){}
+
 
     public Movie(JSONObject jsonObject) throws JSONException {
+
+        backdropPath= jsonObject.getString("backdrop_path");
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating= jsonObject.getDouble("vote_average");
+        movieId =jsonObject.getInt("movieId");
 
     }
 
@@ -32,11 +47,23 @@ public class Movie {
         return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
     }
 
+    public String getBackdropPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s",backdropPath);
+    }
+
     public String getTitle() {
         return title;
     }
 
     public String getOverview() {
         return overview;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public Integer getMovieId() {
+        return movieId;
     }
 }
